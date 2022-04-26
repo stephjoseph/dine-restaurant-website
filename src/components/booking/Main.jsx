799cca84482753ea5a133a4e67adef085307fb91
stephjoseph <sjtmunez@gmail.com>
2022-04-26 14:00:04 +0800

    Update Booking tablet version

--- src/components/booking/Main.jsx
+++ src/components/booking/Main.jsx
@@ -6,8 +6,8 @@ const Main = () => {
   const [numPeople, setNumPeople] = useState(0);
 
   return (
-    <main className="-mt-[8.563rem] mb-[5.375rem] flex w-full flex-col items-center">
-      <form className="flex w-[20.438rem] flex-col gap-8 bg-white p-8 shadow-[0px_75px_100px_-50px_rgba(56,66,85,0.5032)]">
+    <main className="-mt-[8.563rem] mb-[5.375rem] flex w-full flex-col items-center md:mb-[7.5rem] md:-mt-[14.375rem]">
+      <form className="flex w-[20.438rem] flex-col gap-8 bg-white p-8 shadow-[0px_75px_100px_-50px_rgba(56,66,85,0.5032)] md:w-[33.75rem] md:p-12">
         <input
           type="text"
           placeholder="Name"
@@ -18,29 +18,33 @@ const Main = () => {
           placeholder="Email"
           className="w-full border-b border-solid border-[#8E8E8E] pb-4 pl-4 text-lg font-normal tracking-[-0.22px] text-[#111111] placeholder:text-lg placeholder:font-normal placeholder:tracking-[-0.22px] placeholder:text-[#111111]/50 focus:outline-none"
         />
-        <div className="flex flex-col gap-2">
+        <div className="md: flex flex-col gap-2 md:flex-row md:items-center md:gap-[3.25rem]">
           <p className="text-lg font-normal tracking-[-0.22px] text-[#111111]">
             Pick a date
           </p>
           <input
             type="date"
-            className="border border-solid border-[#8E8E8E] p-3"
+            className="border border-solid border-[#8E8E8E] p-3 md:w-[18.063rem]"
           />
         </div>
-        <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-2 md:flex-row md:items-center md:gap-[3.25rem]">
           <p className="text-lg font-normal tracking-[-0.22px] text-[#111111]">
             Pick a time
           </p>
           <input
             type="time"
-            className="border border-solid border-[#8E8E8E] p-3"
+            className="border border-solid border-[#8E8E8E] p-3 md:w-[18.063rem]"
           />
         </div>
-        <div className="flex w-full items-center justify-center gap-16 self-center border-b border-solid border-[#8E8E8E] pb-4">
+        <div className="flex w-full items-center justify-center gap-16 self-center border-b border-solid border-[#8E8E8E] pb-4 md:gap-[7.5rem]">
           <button
             type="button"
-            className="flex h-4 w-4 items-center justify-center"
-            onClick={() => setNumPeople((prevState) => prevState - 1)}
+            className="flex h-4 w-4 items-center justify-center md:h-8 md:w-8"
+            onClick={() =>
+              setNumPeople((prevState) =>
+                prevState <= 0 ? (prevState = 0) : prevState - 1
+              )
+            }
           >
             <img src={iconMinus} alt="" />
           </button>
@@ -49,7 +53,7 @@ const Main = () => {
           </p>
           <button
             type="button"
-            className="flex h-4 w-4 items-center justify-center"
+            className="flex h-4 w-4 items-center justify-center md:h-8 md:w-8"
             onClick={() => setNumPeople((prevState) => prevState + 1)}
           >
             <img src={iconPlus} alt="" />
